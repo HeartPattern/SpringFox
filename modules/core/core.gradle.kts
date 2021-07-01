04a1b96e9153c0dba42a2a34cb72a830824d725f
HeartPattern <heartpattern@heartpattern.io>
2021-07-01 17:29:09 +0900

    CompileOnly paper dependencies

--- modules/core/core.gradle.kts
+++ modules/core/core.gradle.kts
@@ -8,14 +8,14 @@ dependencies {
     implementation("org.jetbrains.kotlinx","kotlinx-coroutines-core","1.5.0")
     implementation("org.jetbrains.kotlinx","kotlinx-coroutines-jdk8","1.5.0")
 
-    implementation("com.destroystokyo.paper", "paper-api", "1.16.5-R0.1-SNAPSHOT")
+    compileOnly("com.destroystokyo.paper", "paper-api", "1.16.5-R0.1-SNAPSHOT")
 
     implementation(project(":common"))
 
     implementation("org.springframework.boot", "spring-boot-starter", "2.4.5")
     implementation("io.github.microutils", "kotlin-logging-jvm", "2.0.8")
 
-    implementation("com.google.auto.service", "auto-service-annotations", "1.0-rc7")
+    compileOnly("com.google.auto.service", "auto-service-annotations", "1.0-rc7")
     kapt("com.google.auto.service", "auto-service", "1.0-rc7")
 }
 
