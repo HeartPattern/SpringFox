c20e48b5cebfda643342652964d9eb66ff0f498f
HeartPattern <heartpattern@heartpattern.io>
2021-07-01 17:26:13 +0900

    Remove shadowjar tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,3 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import com.github.jengelman.gradle.plugins.shadow.transformers.PropertiesFileTransformer
-
-plugins {
-    id("com.github.johnrengelman.shadow") version "7.0.0"
-}
-
 allprojects {
     group = "io.heartpattern.springfox"
     version = "1.0.0-SNAPSHOT"
@@ -14,44 +7,5 @@ subprojects {
     repositories {
         mavenCentral()
         maven("https://repo.heartpattern.io/repository/maven-public")
-    }
-}
-
-evaluationDependsOnChildren()
-
-val allConfiguration = configurations.create("allShadow")
-val partialConfiguration = configurations.create("partialShadow")
-
-dependencies {
-    childProjects.forEach { (name, _) ->
-        allConfiguration(":$name")
-        partialConfiguration(":$name") {
-            isTransitive = false
-        }
-    }
-}
-
-fun ShadowJar.handleSpringFiles(){
-    isZip64 = true
-    mergeServiceFiles()
-    append("META-INF/spring.handlers")
-    append("META-INF/spring.schemas")
-    append("META-INFO/spring.tooling")
-    transform(PropertiesFileTransformer().apply {
-        paths = listOf("META-INF/spring.factories")
-        mergeStrategy = "append"
-    })
-}
-
-tasks {
-    create<ShadowJar>("buildFatPlugin") {
-        handleSpringFiles()
-        configurations = listOf(allConfiguration)
-
-    }
-
-    create<ShadowJar>("buildPlugin") {
-        handleSpringFiles()
-        configurations = listOf(partialConfiguration)
     }
 }
