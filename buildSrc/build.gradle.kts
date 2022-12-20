7694d0927c4c3b076da5451d4b2688c7d4b49a6d
HeartPattern <heartpattern@heartpattern.io>
2022-12-21 01:54:21 +0900

    feat: Bump up kotlin version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,11 +7,11 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin", "kotlin-gradle-plugin", "1.6.21")
-    implementation("org.jetbrains.kotlin", "kotlin-allopen", "1.6.21")
-    implementation("org.jetbrains.kotlin", "kotlin-serialization", "1.6.21")
-    implementation("org.jetbrains.kotlin", "kotlin-gradle-plugin", "1.6.21")
-    implementation("org.jetbrains.dokka", "dokka-gradle-plugin", "1.6.21")
+    implementation("org.jetbrains.kotlin", "kotlin-gradle-plugin", "1.7.21")
+    implementation("org.jetbrains.kotlin", "kotlin-allopen", "1.7.21")
+    implementation("org.jetbrains.kotlin", "kotlin-serialization", "1.7.21")
+    implementation("org.jetbrains.kotlin", "kotlin-gradle-plugin", "1.7.21")
+    implementation("org.jetbrains.dokka", "dokka-gradle-plugin", "1.7.20")
     implementation("org.springframework.boot", "spring-boot-gradle-plugin", "2.7.0")
     implementation("io.spring.gradle", "dependency-management-plugin", "1.0.11.RELEASE")
     implementation("io.github.gradle-nexus","publish-plugin","1.1.0")
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,9 +1,9 @@
 object Versions {
-    val PAPER = "1.18.2-R0.1-SNAPSHOT"
-    val KOTLIN = "1.6.21"
-    val KOTLIN_COROUTINE = "1.6.2"
+    val PAPER = "1.19.2-R0.1-SNAPSHOT"
+    val KOTLIN = "1.7.21"
+    val KOTLIN_COROUTINE = "1.6.4"
     val KOTLIN_LOGGING = "2.1.23"
-    val DOKKA = "1.6.2"
+    val DOKKA = "1.7.20"
     val AUTO_SERVICE = "1.0.1"
     val SPRING = "2.7.0"
 }
